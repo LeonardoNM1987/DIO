7131e345f55792bdcbe3ab284510cf20c1c72a6e
Leonardo Mendonça <leonardonm1987@gmail.com>
2021-10-12 11:41:50 -0300

    novas alterações

--- competicao.php
+++ competicao.php
@@ -1,3 +1,6 @@
+<?php session_start(); ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,67 +12,83 @@
 </head>
 <body>
 	
-	<form action="competicao.php" method="post">
-
+	<form action="#" method="post">
 		<fieldset>
 			<legend>Competidores</legend>
+			
 			<li><label for="nome">Nome:</label>
-			<input type="text" name="nome" id="nome" maxlength="50" minlength="2" required><br></li>
+			<input type="text" name="nome" id="nome"><br></li>
+			
 			<li><label for="idade">Idade:</label>
-			<input type="number" name="idade" id="idade" min=0 max=120 required><br></li>
-			<input type="submit" value="Enviar" id="subm" for='enviar'>
+			<input type="text" name="idade" id="idade"><br></li>
+			
+			<input type="submit" value="Enviar" id="subm">
+			
 		</fieldset>
 	</form>
-	<hr>
+	
 	<?php
+				
+		$mensagemErro = isset($_SESSION['mensagem-erro']) ? $_SESSION['mensagem-erro'] : "";
+		echo $mensagemErro;
+						
+	?>
+	
+	<hr>
+<?php
 
-	
-	
-	
-
-if((isset($_POST['nome'])) && (isset($_POST['idade']))){
-	
-	$nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
-	$idade = filter_var($_POST['idade'], FILTER_SANITIZE_NUMBER_INT);
-	
-	echo "Nome: $nome <br>";
-	echo "Idade: $idade <br>";
-	
-	switch($idade){
-		case ($idade < 6):
-			echo "Menores de 6 anos não podem competir!";
-			break;
-		case ($idade >= 6 && $idade <=12):
-			echo "Categoria: Infantil!";
-			break;
-		case ($idade >= 13 && $idade <=18 ):
-			echo "Categoria: Adolescente!";
-			break;
-		case ($idade > 18):
-			echo "Categoria: Adulto!";
-			break;
-		default:
-			echo "Não foi possível definir a categoria!";
-			break;
+		
+	if(empty($_POST['nome'])){
+		$_SESSION['mensagem-erro'] = "O nome não pode ser vazio!<br>";
+	}
+	if(empty($_POST['idade'])){
+		$_SESSION['mensagem-erro'] = "A idade não pode ser vazia!<br>";
 	}	
 
-}else{
-	echo "Preencha todos os campos!";
-}
 
+
+
+if((!empty($_POST['nome'])) && (!empty($_POST['idade']))){
+			
+		$nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
+		$idade = filter_var($_POST['idade'], FILTER_SANITIZE_NUMBER_INT);
+		
+		echo "Nome: $nome (".gettype($nome).")<br>";
+		echo "Idade: $idade (".gettype($idade).")<br>";
+				
+
+
+		switch($idade){
+			case ($idade < 6):
+				echo "Menores de 6 anos não podem competir!";
+				break;
+			case ($idade >= 6 && $idade <=12):
+				echo "Categoria: Infantil!";
+				break;
+			case ($idade >= 13 && $idade <=18 ):
+				echo "Categoria: Adolescente!";
+				break;
+			case ($idade > 18):
+				echo "Categoria: Adulto!";
+				break;
+			default:
+				echo "Não foi possível definir a categoria!";
+				break;
+		}			
+
+		unset($_SESSION['mensagem-erro']);
+
+	}else{
+		$_SESSION['mensagem-erro'] = "Verifique os dados e tente novamente!";		
+	}
 
 	
-		
-		
-		
-		
-
-		
 
 
 
 
-	?>
+
+?>
 
 
 
